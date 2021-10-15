7a4db3684bed37bbbcb9ceca0c0ea745d3b71b22
Adam Angell <Adam.Angell@networkingb2a.com>
2021-10-15 02:57:44 +0000

    added name and main to bypass env vars

--- Flask_web_app/first_web_app.py
+++ Flask_web_app/first_web_app.py
@@ -4,4 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return """ <h1>Hello, World!</h1>
+                I am now a WebNetDevEngineer.
+                Just designing an super neat webpage.
+                Just part of learn every day idea.
+                I just added a export FLASK_DEBUG=1 so i dont have to restart the service every change """
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
