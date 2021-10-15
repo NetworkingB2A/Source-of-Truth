765f3ab9abe67c8ee3c7d98503332b0f77833a92
Adam Angell <Adam.Angell@networkingb2a.com>
2021-10-15 03:03:09 +0000

    added a decorator for home

--- Flask_web_app/first_web_app.py
+++ Flask_web_app/first_web_app.py
@@ -3,13 +3,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+@app.route("/home")
+def home():
     return """ <h1>Hello, World!</h1>
                 I am now a WebNetDevEngineer.
                 Just designing an super neat webpage.
                 Just part of learn every day idea.
                 I just added a export FLASK_DEBUG=1 so i dont have to restart the service every change """
 
+@app.route("/about")
+def about():
+    return """ <h1>About my app, so far very basic."""
 
 if __name__ == '__main__':
     app.run(debug=True)
